3a95650741381f5a0c62a1622ee53e451258278e
Giovanny Orozco <giovannyorozco4000@gmail.com>
2026-06-22 18:33:47 -0500

    feat: agregar lenguajes vim, markdown y markdown_inline al plugin nvim-treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,33 +1,31 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	lazy = false,        -- NO usar carga diferida
-	build = ':TSUpdate', -- Actualiza los parsers al instalar/actualizar
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
+  config = function()
+    require('nvim-treesitter').setup {
+      install_dir = vim.fn.stdpath('data') .. '/site',
+    }
 
-	config = function()
-		-- Configuración del plugin
-		require('nvim-treesitter').setup {
-			install_dir = vim.fn.stdpath('data') .. '/site',
-		}
+    -- Se instalan automáticamente al abrir nvim
+    require('nvim-treesitter').install({
+      'lua', 'python', 'javascript', 'typescript', 'rust',
+			'c', 'cpp', 'html', 'css', 'json', 'bash', 'php', 'java',
+      'regex', 'vim', 'markdown', 'markdown_inline',
+    })
 
-		-- Parsers a instalar (agrega o quita los que necesites)
-		require('nvim-treesitter').install({
-			'lua', 'python', 'javascript', 'typescript', 'rust', 'c',
-			'cpp', 'html', 'css', 'json', 'bash', 'php', 'java', 'regex'
-		})
-
-		-- Habilitar funciones por tipo de archivo
-		vim.api.nvim_create_autocmd('FileType', {
-			pattern = {
-				'lua', 'python', 'javascript', 'typescript', 'rust', 'c',
-				'cpp', 'html', 'css', 'json', 'bash', 'php', 'java'
-			},
-			callback = function()
-				-- Resaltado de sintaxis
-				vim.treesitter.start()
-
-				-- Sangría experimental
-				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-			end,
-		})
-	end,
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = {
+        'lua', 'python', 'javascript', 'typescript', 'rust',
+				'c', 'cpp', 'html', 'css', 'json', 'bash', 'php',
+				'java', 'regex', 'vim', 'markdown',
+      },
+      callback = function()
+        local ok = pcall(vim.treesitter.start)
+        if ok then
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      end,
+    })
+  end,
 }
